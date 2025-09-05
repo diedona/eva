092eb9e0a35b5f282a2eaf58b674105273523f09
diedona <diedona@gmail.com>
2025-09-05 10:31:30 -0300

    add swagger

--- src/Eva.Catalog/Eva.Catalog.Api/Program.cs
+++ src/Eva.Catalog/Eva.Catalog.Api/Program.cs
@@ -3,32 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapOpenApi();
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
-}
+    options.SwaggerEndpoint("/openapi/v1.json", "Eva Catalog v1");
+});
+
+app.MapGet("/api/hello", () =>
+{
+    return "Hello world!";
+});
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/hello", () =>
-{
-    string[] greetings = ["hi!", "hello there!"];
-    return Results.Ok(greetings);
-})
-.WithName("hello");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
