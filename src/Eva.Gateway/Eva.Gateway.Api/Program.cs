b0c30eb96b092dc47f31904c1596e55ba4e819e8
Diego Doná <diedona@gmail.com>
2025-09-10 16:10:45 -0300

    start yarp configuration

--- src/Eva.Gateway/Eva.Gateway.Api/Program.cs
+++ src/Eva.Gateway/Eva.Gateway.Api/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("Yarp"));
+
+builder.Host.UseSerilog((ctx, config) => config.ReadFrom.Configuration(ctx.Configuration));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -9,10 +15,14 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging();
+
+app.UseRouting();
+
+app.UseHttpsRedirection();
 
 app.MapOpenApi();
 
-app.UseHttpsRedirection();
+app.MapReverseProxy();
 
 app.Run();
