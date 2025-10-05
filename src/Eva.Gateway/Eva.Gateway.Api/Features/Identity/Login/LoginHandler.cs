1a43d9df6b5ff408b6a666b6b4feef97ae6e2cf8
Diego Doná <diedona@gmail.com>
2025-10-05 08:52:58 -0300

    add logger in program handler

--- src/Eva.Gateway/Eva.Gateway.Api/Features/Identity/Login/LoginHandler.cs
+++ src/Eva.Gateway/Eva.Gateway.Api/Features/Identity/Login/LoginHandler.cs
@@ -3,20 +3,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Eva.Gateway.Api.Features.Identity.Login;
 
-public static class LoginHandler
+internal static class LoginHandler
 {
     public static async Task<Results<
         Ok<string>,
         BadRequest
     >> HandleAsync(
         HttpContext context,
+        ILogger<Program> log,
         CancellationToken cancellationToken,
-        [FromBody] LoginRequest request
+        LoginRequest request
     )
     {
+        log.LogInformation("@{request}", request);
         return TypedResults.Ok($"oh, hello there {request.Username} =)");
     }
 }
